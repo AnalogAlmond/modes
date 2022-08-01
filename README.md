1ac5bd35ebb3c46a963c6516f9019e6456d18fb1
aba053157 <aba053157@gmail.com>
2022-08-01 17:54:34 +0300

    added multiple key functionality, updated README

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # modes
 generates a random list of modes and plays the backing tracks with mocp
-## backing tracks
-you can provide your own backing tracks. each key has its own folder with files for each mode(eg. c/phrygian.mp3 e/dorian.mp3 etc) 
 ## usage
 run ./modes
+## backing tracks
+you can provide your own backing tracks. each key can have its own folder with files for each mode(eg. c/phrygian.mp3 e/dorian.mp3 etc) 
+or disable backing tracks by commenting out the lines where the "system()" function is called
+### default keys
+c, g, d, a, e, b, f#, c#, ab, eb, bb, f
+### default modes
+ionian, dorian, phrygian, lydian, mixolydian, aeolian, locrian
+
--- modes.cpp
+++ modes.cpp
@@ -3,26 +3,69 @@
 
 using namespace std;
 
+#define MODES 7
+#define COMBINATIONS 84
+
+void userInput(string& key);
+
 int main(){
-    string key = "c";//todo: add other keys
+    string keys[] = {"c", "g", "d", "a", "e", "b", "f#", "c#", "ab", "eb", "bb", "f"};
     string mode[] = {"ionian", "dorian", "phrygian", "lydian", "mixolydian", "aeolian", "locrian"};
+    int used[MODES];
+    string usedcombos[COMBINATIONS];
+    string command;
+    string key;
+    int scales;
 
     srand(time(0));//takes the current time as a seed
-    int r = (rand()%7);//generates a random number between 0 and 6
-    int used[7];
-    string file;
-    system("mocp --server");
+    int r = rand()%7;//generates a random number between 0 and 6
+    int rkey = rand()%12;//between 0 and 11
     
-    for(int i = 0; i < 7; i++){
-        used[i] = r;
-        cout << mode[r] << '\n';
-        file = "mocp --enqueue " + key + '/' + mode[r] + ".mp3";//command to get the file into the queue
-        system(file.c_str());//turns it into a constant char* and runs the command
-        if(i == 6) break;
-        while(find(begin(used), end(used), r)!=end(used)){
-            r = (rand()%7);
+    system("mocp --exit");
+    system("mocp --server");
+    userInput(key);
+    
+    //if the user only wants one key:
+    if(find(begin(keys), end(keys), key)!=end(keys)){
+        for(int i = 0; i < MODES; i++){
+            used[i] = r;
+            cout << mode[r] << '\n';
+            command = "mocp --enqueue " + key + '/' + mode[r] + ".mp3";//command to get the file into the queue
+            system(command.c_str());//turns it into a constant char* and runs the command
+            if(i == 6) break;
+            while(find(begin(used), end(used), r)!=end(used)){
+                r = rand()%7;
+            }
         }
     }
+    //if the user wants multiple keys:
+    else if(strcmp(key.c_str(), "all") == 0){
+        cout << "how many scales would you like in total? max=" << COMBINATIONS << '\n';
+        cin >> scales;
+        for(int i = 0; i < scales; i++){
+           usedcombos[i]=keys[rkey] + "/" + mode[r];
+           cout << usedcombos[i] << '\n';
+           command = "mocp --enqueue " + usedcombos[i] + ".mp3";//command to get the file into the queue
+           system(command.c_str());//turns it into a constant char* and runs the command
+           if(i == (scales-1)) break;
+           while(find(begin(usedcombos), end(usedcombos), keys[rkey] + "/" + mode[r])!=end(usedcombos)){
+                r = rand()%7;
+                rkey = rand()%12;
+           }
+       }
+    }
+    else{
+        return 1;
+    }
+    
     system("mocp --play");//starts the playlist
     return 0;
+}
+
+
+void userInput(string& key){
+    cout << "what key would you like?\n";
+    cout << "c, g, d, a, e, b, f#, c#, ab, eb, bb, f\n";
+    cout << "type 'all' for all keys\n";
+    cin >> key;
 }
